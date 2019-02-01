f0888caeac51e7ec19f728b6e5ad9ba158a7078a
Simon HAJEK <simonhajek88@gmail.com>
2019-02-01 21:52:18 +0100

    Articles

--- GLA_auctions/GLA_auctions-ejb/build/generated-sources/ap-source-output/model/Articles_.java
+++ GLA_auctions/GLA_auctions-ejb/build/generated-sources/ap-source-output/model/Articles_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-01-31T12:34:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-02-01T21:29:18")
 @StaticMetamodel(Articles.class)
 public class Articles_ { 
 
